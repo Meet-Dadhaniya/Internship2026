d73e49bd4d2aa2558207062bf17c614c15c0cfd3
Meet-Dadhaniya <patelmeet2669@gmail.com>
2026-06-15 15:03:55 +0530

    Created image slider

---
+++ Day4_Image_slider/script.js
@@ -0,0 +1,20 @@
+const slides = document.getElementById('slides');
+const totalSlides = slides.children.length;
+let currentIndex = 0;
+
+function updateSlide() {
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  updateSlide();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+  updateSlide();
+}
+
+// Auto slide every 4 seconds
+setInterval(nextSlide, 4000);
